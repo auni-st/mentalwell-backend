b222714d24a648079fdea0bd2745ed5ec6edf157
Ahmad Ramadhan Auni <aramadhan395@gmail.com>
2023-11-14 14:30:20 +0700

    disable build

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ app.get('/api', (req, res) => {
 });
 
 app.get('/user', (req, res) => {
-  res.send('Hello, this is for showing all changed 12345600000');
+  res.send('Hello, this is for showing all changed 12345600000 I CHANGED SOMETHING');
 });
 
 const PORT = process.env.PORT || 3000;
