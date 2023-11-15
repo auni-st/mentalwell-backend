acf543506d5e23c90328ff50f708e3d856d4f43d
Ahmad Ramadhan Auni <aramadhan395@gmail.com>
2023-11-15 09:52:01 +0700

    test body-parser

--- api/index.js
+++ api/index.js
@@ -15,21 +15,21 @@ app.get('/items', (req, res) => {
   res.json(items);
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello, Vercel and Express.js!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello, Vercel and Express.js!');
+// });
 
-app.get('/api', (req, res) => {
-  res.send('Hello, this is API changed againnn');
-});
+// app.get('/api', (req, res) => {
+//   res.send('Hello, this is API changed againnn');
+// });
 
-app.get('/user', (req, res) => {
-  res.send('Hello, this is for showing all changed 12345600000 I CHANGED SOMETHING');
-});
+// app.get('/user', (req, res) => {
+//   res.send('Hello, this is for showing all changed 12345600000 I CHANGED SOMETHING');
+// });
 
-app.get('/auni', (req, res) => {
-  res.send('Hello, this is auni. ADA YANG BERUBAH');
-});
+// app.get('/auni', (req, res) => {
+//   res.send('Hello, this is auni. ADA YANG BERUBAH');
+// });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
