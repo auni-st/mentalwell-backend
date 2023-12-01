d45574f4c109e990a6f3aa97fa574a659bcd8a85
Ahmad Ramadhan Auni <aramadhan395@gmail.com>
2023-12-01 20:19:35 +0700

    add cors

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,6 +15,12 @@ const userRoutes = require('./routes/userRoutes');
 const patientRoutes = require('./routes/patientRoutes');
 const psychologistRoutes = require('./routes/psychologistRoutes');
 const articleRoutes = require('./routes/articleRoutes');
+
+//set CORS
+// Use CORS middleware with the desired options
+app.use(cors({
+  origin: '*',
+}));
 
 //use routes
 app.use('/', authRoutes);
